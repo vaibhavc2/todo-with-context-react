7f6f89c81794a3f8b9d6ccfceb59e1d2fab461cf
Vaibhav Chopra <73025956+vaibhavc2@users.noreply.github.com>
2023-10-07 11:43:22 +0530

    basic

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+  // darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
